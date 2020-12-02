039fe47ddb094ef689852948d472e61142a487cc
Rupert Saxton <rupertsaxton@gmail.com>
2020-12-02 08:20:29 -0500

    Day 2

---
+++ 2/2.rb
@@ -0,0 +1,27 @@
+file = File.open('input2.txt')
+file_data = file.readlines.map(&:chomp).map(&:split)
+
+inputs = file_data.map do |line|
+  lower, upper = line[0].split('-')
+  letter = line[1].chomp(':')
+  password = line[2]
+  { lower: lower.to_i, upper: upper.to_i, letter: letter, password: password }
+end
+
+def part_1_rules(inputs)
+  valid_inputs = inputs.select do |input|
+    range = (input[:lower]..input[:upper])
+    range.include?(input[:password].count(input[:letter]))
+  end
+end
+
+def part_2_rules(inputs)
+  valid_inputs = inputs.select do |input|
+    first = input[:password][input[:lower] - 1] == input[:letter] && input[:password][input[:upper] - 1] != input[:letter]
+    second = input[:password][input[:upper] - 1] == input[:letter] && input[:password][input[:lower] - 1] != input[:letter]
+    first ^ second
+  end
+end
+
+puts part_1_rules(inputs).length
+puts part_2_rules(inputs).length
