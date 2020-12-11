851b9b499f069c0a5ab928f565c03b313e51e530
Rupert Saxton <rupertsaxton@gmail.com>
2020-12-10 23:15:22 -0500

    Day 7 part 2

--- 7/7.rb
+++ 7/7.rb
@@ -9,23 +9,49 @@ data.each do |line|
   key = line.match(/\A(?<bag_type>.*) bags contain/)[:bag_type]
   results = line.scan(/\d .* bags?/)
   results = results.length > 0 ? results[0].split(', ') : results
-  bags[key] = results.map { |x| x.match(/\d (?<bag_type>.*) bag/)[:bag_type] }
+  bags[key] = results.map do |x|
+    match = x.match(/(?<quantity>\d) (?<bag_type>.*) bag/)
+    [match[:bag_type], match[:quantity].to_i]
+  end
 end
 
-bags_containing_shiny_gold_bags = []
+# puts "#{bags}"
 
-bags_to_check = ["shiny gold"]
+# puts bags.length
 
-while bags_to_check.length > bags_containing_shiny_gold_bags.length do
-  bags_containing_shiny_gold_bags = bags_to_check.dup
-  bags.each do |k, v|
-    if (bags_to_check & v).length > 0
-      bags_to_check << k
+# puts bags_containing_shiny_gold_bags.to_set.length
+
+class Tree
+  attr_accessor :nodes, :value, :quantity
+
+  def initialize(value, quantity)
+    @value = value
+    @quantity = quantity
+    @nodes = []
+  end
+end
+
+def build_tree(bags, root)
+  tree = Tree.new(*root)
+  if bags[root[0]]
+    bags[root[0]].each do |bag|
+      tree.nodes << build_tree(bags, bag)
     end
   end
-  bags_to_check = bags_to_check.to_set.to_a
+  tree
 end
 
-puts bags.length
+def count_bags(tree)
+  return tree.quantity if tree.nodes == []
+  node_sum = tree.nodes.sum do |node|
+    (tree.quantity * count_bags(node))
+  end
+  tree.quantity + node_sum
+end
 
-puts bags_containing_shiny_gold_bags.to_set.length
+def part2(bags)
+  tree = build_tree(bags, ["shiny gold", 1])
+  count_bags(tree) - 1
+end
+
+puts part2(bags)
